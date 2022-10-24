b72dc86c0c68d050883af0da7337a0f0c9e3f036
a.rafaliarison <aymar.rafaliarison@pulse.mg>
2022-10-24 13:35:29 +0300

    modif database local to mongo atlas

--- connection.js
+++ connection.js
@@ -1,30 +1,30 @@
-const {MongoClient} = require('mongodb')
+const { MongoClient } = require('mongodb')
 const mongoose = require('mongoose')
 
 // const connexMongo = "mongodb+srv://kynai:mongo$12HzXHZ@cluster0.vuekb.mongodb.net/kaly?retryWrites=true&w=majority"
-const connexMongo = "mongodb://localhost:27017/kaly"
-function getDb(){
+const connexMongo = "mongodb+srv://kynai:mongo$12HzXHZ@cluster0.vuekb.mongodb.net/kaly"
+function getDb() {
     return MongoClient.connect(connexMongo, { useUnifiedTopology: true })
-    .then((client) => {
-        const db = client.db('kaly')
-        return db;
-    })
+        .then((client) => {
+            const db = client.db('kaly')
+            return db;
+        })
 }
 
-function connectMongoose(){
+function connectMongoose() {
     return mongoose.connect(connexMongo, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
 }
 
-function genererToken(){
+function genererToken() {
     var result = '';
     var motCLe = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var motCrypte = motCLe.length;
-    for ( var i = 0; i < 43; i++ )
+    for (var i = 0; i < 43; i++)
         result += motCLe.charAt(Math.floor(Math.random() * motCrypte));
     return result;
 }
 
-module.exports = {connex:getDb, token:genererToken, conMongoose : connectMongoose}
+module.exports = { connex: getDb, token: genererToken, conMongoose: connectMongoose }
