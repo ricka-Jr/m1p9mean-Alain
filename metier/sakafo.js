d87b2ab937dc5fd482a118c33d293187c0766552
ricka-Jr <80506942+ricka-Jr@users.noreply.github.com>
2022-10-24 19:45:12 +0300

    ajout 's' sur le table

--- metier/sakafo.js
+++ metier/sakafo.js
@@ -1,7 +1,7 @@
 var sakafo = require('../model/SakafoModel')
 
 function findByIdRestaurant(req, res){
-    sakafo.find({"restaurant._id" : req.params.id}).exec((err, data) => {
+    sakafo.find({"restaurants._id" : req.params.id}).exec((err, data) => {
         if (err) {
             console.log(err);
             res.status(400).send({ message: 'ERREUR SERVEUR' });
